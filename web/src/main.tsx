3a24113aeb80dd9690e50435961394dda4100d7e
rreno1 <rrenomeronjr@gmail.com>
2026-08-25 19:45:05 +0800

    feat: provide app-wide toast notifications

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import { AppErrorBoundary } from './app/AppErrorBoundary';
+import { ToastProvider } from './app/ToastProvider';
 import { AuthProvider } from './features/auth/AuthProvider';
 import './styles/global.css';
 
@@ -14,9 +15,11 @@ if (!rootElement) {
 createRoot(rootElement).render(
   <StrictMode>
     <AppErrorBoundary>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <ToastProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ToastProvider>
     </AppErrorBoundary>
   </StrictMode>,
 );
